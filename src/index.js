bdb80e080829b646c1c8f7cec4db97b41fc55ab4
roncohen <ron.cohen.job@gmail.com>
2025-07-05 22:09:47 +0300

    deploy 2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Root} from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
 
 const root: Root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+    <BrowserRouter basename="/hour-calculator">
+        <App />
+    </BrowserRouter>
 );
 
 reportWebVitals();
